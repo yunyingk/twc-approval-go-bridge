c5aef9cb81cbd66c2576e8327f7af1584a82fe07
yunyingk <haoda2195176187@gmail.com>
2026-09-18 09:53:06 +0800

    chore: initialize Go service project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yunyingk/twc-approval-go-bridge
+
+go 1.24
---
+++ internal/version/version.go
@@ -0,0 +1,5 @@
+// Package version exposes build-time version information.
+package version
+
+// Version is replaced at build time. It intentionally defaults to dev for local runs.
+var Version = "dev"
